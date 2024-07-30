212578c248ce0da86ce9004e46dd9ffe6d75db07
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-07-30 08:01:57 +0400

    Update page.js

--- pantryapp/app/page.js
+++ pantryapp/app/page.js
@@ -10,7 +10,21 @@ export default function Home() {
       justifyContent={"center"}
       alignItems={"center"}
     >
-      <Stack width="800px" height="600px" spacing={2}></Stack>
+      <Stack width="800px" height="600px" spacing={2}>
+        {item.map((i) => (
+          <Box
+            key={i}
+            width="100%"
+            height="100px"
+            display={"flex"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            bgcolor={"#f0f0f0"}
+          >
+            {i}
+          </Box>
+        ))}
+      </Stack>
     </Box>
   );
 }
