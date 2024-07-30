3de3ff3a1cff7c6bb6cb00116b3bf12fe4ff1f70
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-07-30 09:19:41 +0400

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBesiicHNaBynCU_XcsbhQ-90sJTT2a-Ww",
+  authDomain: "pantryapp-252c3.firebaseapp.com",
+  projectId: "pantryapp-252c3",
+  storageBucket: "pantryapp-252c3.appspot.com",
+  messagingSenderId: "941642895099",
+  appId: "1:941642895099:web:db45effb5cbb2f2d336a5e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
