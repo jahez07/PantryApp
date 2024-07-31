31d0c90b03b6fbb216e48b761a10934129566750
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-07-31 07:08:01 +0400

    Update page.js

--- pantryapp/app/page.js
+++ pantryapp/app/page.js
@@ -1,32 +1,45 @@
-'use client'
+"use client";
 import { firestore } from "@/firebase";
-import { Box, Button, Stack, Typography } from "@mui/material";
+import { Box, Button, Stack, Typography, Modal } from "@mui/material";
 import { Firestore, getDocs } from "firebase/firestore";
 import { collection } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { query } from "firebase/firestore";
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'white',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
 
 export default function Home() {
-  const [pantry, setPantry] = useState([])
+  const [pantry, setPantry] = useState([]);
 
   const [open, setOpen] = useState(false);
+  const [itemName, setItemName] = useState('')
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
   useEffect(() => {
     const updatePantry = async () => {
-      const snapshot = query(collection(firestore, 'pantry'))
-      const docs = await  getDocs(snapshot)
-      const pantryList = []
+      const snapshot = query(collection(firestore, "pantry"));
+      const docs = await getDocs(snapshot);
+      const pantryList = [];
       docs.forEach((doc) => {
-        pantryList.push(doc.id)
-      })
-      console.log(pantryList)
-      setPantry(pantryList)
-    }
-    updatePantry()
-  }, [])
+        pantryList.push(doc.id);
+      });
+      console.log(pantryList);
+      setPantry(pantryList);
+    };
+    updatePantry();
+  }, []);
   return (
     <Box
       width="100vw"
@@ -37,7 +50,24 @@ export default function Home() {
       alignItems={"center"}
       gap={2}
     >
-      <Button variant="contained">Add Item</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Would you like to add any items to the pantry?
+          </Typography>
+        </Box>
+      </Modal>
+      <Button variant="outlined" onClick={handleOpen}>
+        Add Item
+      </Button>
       <Box border={"1px solid #333"}>
         <Box
           width="800px"
